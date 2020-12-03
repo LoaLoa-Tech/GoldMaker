93a29861b90a17f7d57fc23ae7a8a2cd7675c790
Robert Tran <truongduchuy910@gmail.com>
2020-12-03 16:29:47 +0700

    done upload router

---
+++ routers/upload-vi.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var router = express.Router();
+var formidable = require("formidable");
+const form = formidable({
+  uploadDir: "./public/upload-vi",
+  keepExtensions: true,
+});
+var xss = require("xss");
+const { throws } = require("assert");
+router.get("/upload-vi", (req, res) => {
+  res.render("upload-vi", { message: null });
+});
+router.post("/upload-vi", (req, res, next) => {
+  var message = "";
+  /**
+   * REMOVE ALL FILE
+   */
+  fs.readdirSync(path.join(__dirname, "../public/upload-vi")).forEach(
+    (file) => {
+      var rm = path.join(__dirname, "../public/upload-vi", file);
+      fs.unlinkSync(rm);
+    }
+  );
+  /**
+   * PARSE
+   */
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.render("upload-vi", { message: "Upload không thành công." });
+      next();
+    } else {
+      try {
+        console.log(files.file);
+        fs.renameSync(
+          files.file.path,
+          path.join(__dirname, "../public/upload-vi/", files.file.name)
+        );
+        res.render("upload-vi", { message: xss(message) });
+      } catch {
+        next();
+      }
+    }
+  });
+});
+module.exports = router;
