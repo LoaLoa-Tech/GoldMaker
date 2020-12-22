e26a2bab13ff52fc35404405630b9816b02644df
root <root@vultr.guest>
2020-12-22 02:17:32 +0000

    server update

--- routers/upload-en.js
+++ routers/upload-en.js
@@ -4,51 +4,52 @@ var fs = require("fs");
 var router = express.Router();
 var formidable = require("formidable");
 const form = formidable({
-  uploadDir: "./public/upload-en",
-  keepExtensions: true,
+	uploadDir: "./public/upload-en",
+	keepExtensions: true,
 });
 var xss = require("xss");
 const { throws } = require("assert");
 router.get("/upload-en", (req, res) => {
-  if (req.session.isAuthed == true) {
-    res.render("upload-en", { message: null, data: null });
-  } else {
-    res.redirect("/login");
-  }
+	if (req.session.isAuthed == true) {
+		res.render("upload-en", { message: null, data: null });
+	} else {
+		res.redirect("/login");
+	}
 });
 router.post("/upload-en", (req, res, next) => {
-  var message = "";
-  /**
-   * REMOVE ALL FILE
-   */
-  try {
-    fs.readdirSync(path.join(__dirname, "../public/upload-en")).forEach(
-      (file) => {
-        var rm = path.join(__dirname, "../public/upload-en", file);
-        fs.unlinkSync(rm);
-      }
-    );
-  } catch (e) {
-    console.log("cannot remove file");
-  }
-  /**
-   * PARSE
-   */
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.render("upload-en", { message: "Upload không thành công." });
-      next();
-    } else {
-      try {
-        fs.renameSync(
-          files.file.path,
-          path.join(__dirname, "../public/upload-en/", files.file.name)
-        );
-        res.render("upload-en", { message: xss(message) });
-      } catch {
-        next();
-      }
-    }
-  });
+	var message = "";
+	/**
+	 * REMOVE ALL FILE
+	 */
+	try {
+		fs.readdirSync(path.join(__dirname, "../public/upload-en")).forEach(
+			(file) => {
+				var rm = path.join(__dirname, "../public/upload-en", file);
+				fs.unlinkSync(rm);
+			}
+		);
+	} catch (e) {
+		console.log("cannot remove file");
+		message = "cannot remove file";
+	}
+	/**
+	 * PARSE
+	 */
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			message = "upload fail";
+		} else {
+			try {
+				fs.renameSync(
+					files.file.path,
+					path.join(__dirname, "../public/upload-en/", files.file.name)
+				);
+				message = "success";
+			} catch {
+				message = "done";
+			}
+		}
+	});
+	res.render("upload-en", { message: xss(message),data:null });
 });
 module.exports = router;
--- routers/upload-vi.js
+++ routers/upload-vi.js
@@ -4,51 +4,52 @@ var fs = require("fs");
 var router = express.Router();
 var formidable = require("formidable");
 const form = formidable({
-  uploadDir: "./public/upload-vi",
-  keepExtensions: true,
+	uploadDir: "./public/upload-vi",
+	keepExtensions: true,
 });
 var xss = require("xss");
 const { throws } = require("assert");
 router.get("/upload-vi", (req, res) => {
-  if (req.session.isAuthed == true) {
-    res.render("upload-vi", { message: null, data: null });
-  } else {
-    res.redirect("/login");
-  }
+	if (req.session.isAuthed == true) {
+		res.render("upload-vi", { message: null, data: null });
+	} else {
+		res.redirect("/login");
+	}
 });
 router.post("/upload-vi", (req, res, next) => {
-  var message = "";
-  /**
-   * REMOVE ALL FILE
-   */
-  try {
-    fs.readdirSync(path.join(__dirname, "../public/upload-vi")).forEach(
-      (file) => {
-        var rm = path.join(__dirname, "../public/upload-vi", file);
-        fs.unlinkSync(rm);
-      }
-    );
-  } catch (e) {
-    console.log("cannot remove file");
-  }
-  /**
-   * PARSE
-   */
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.render("upload-vi", { message: "Upload không thành công." });
-      next();
-    } else {
-      try {
-        fs.renameSync(
-          files.file.path,
-          path.join(__dirname, "../public/upload-vi/", files.file.name)
-        );
-        res.render("upload-vi", { message: xss(message) });
-      } catch {
-        next();
-      }
-    }
-  });
+	var message = "";
+	/**
+	 * REMOVE ALL FILE
+	 */
+	try {
+		fs.readdirSync(path.join(__dirname, "../public/upload-vi")).forEach(
+			(file) => {
+				var rm = path.join(__dirname, "../public/upload-vi", file);
+				fs.unlinkSync(rm);
+			}
+		);
+	} catch (e) {
+		console.log("cannot remove file");
+		message = "cannot remove file";
+	}
+	/**
+	 * PARSE
+	 */
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			message = "upload fail";
+		} else {
+			try {
+				fs.renameSync(
+					files.file.path,
+					path.join(__dirname, "../public/upload-vi/", files.file.name)
+				);
+				message = "success";
+			} catch {
+				message = "done";
+			}
+		}
+	});
+	res.render("upload-vi", { message: xss(message),data:null });
 });
 module.exports = router;
